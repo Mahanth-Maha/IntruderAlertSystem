ac4d6ab2f9c07fd6423355213d4df18619c1db17
maha <mahanthysree@gmail.com>
2022-11-08 11:59:09 +0000

    Auto-Format...hehehe

--- ID_V2_pi/Rpi_Intruder_Detect_v1.py
+++ ID_V2_pi/Rpi_Intruder_Detect_v1.py
@@ -19,13 +19,15 @@ Pi_MAddr = 'PiMail@gmail.com'
 Pi_MPswd = 'Pswd'
 Client_Maddr = 'client@gmail.com'
 
+
 def SendMail(images):
     mail = MIMEMultipart()
     mail['Subject'] = 'COMMANDER WE HAVE AN INTRUDER !'
-    mail['From'] =  Pi_MAddr
+    mail['From'] = Pi_MAddr
     mail['To'] = Client_Maddr
 
-    text = MIMEText("Sir,\n\n\tWe have an Intruder, please take a look ! here are pictures...\n\n\n Happy Hunting...John Wick")
+    text = MIMEText(
+        "Sir,\n\n\tWe have an Intruder, please take a look ! here are pictures...\n\n\n Happy Hunting...John Wick")
     mail.attach(text)
 
     for ImgFileName in images:
@@ -33,12 +35,12 @@ def SendMail(images):
             img_data = f.read()
         image = MIMEImage(img_data, name=os.path.basename(ImgFileName))
         mail.attach(image)
-    
+
     s = smtplib.SMTP('smtp.gmail.com', 587)
     s.ehlo()
     s.starttls()
     s.ehlo()
-    s.login(Pi_MAddr,Pi_MPswd)
+    s.login(Pi_MAddr, Pi_MPswd)
     s.sendmail(Pi_MAddr, Client_Maddr, mail.as_string())
     s.quit()
 
@@ -50,25 +52,20 @@ while True:
         tm = datetime.datetime.now().strftime('%Y-%m-%d%H:%M:%S')
         images = []
         taken = 0
-        for taken in range(No_of_Pictures):    
-            print("Picture "+str(taken) +"... And Smile...",end = ' ')
+        for taken in range(No_of_Pictures):
+            print("Picture "+str(taken) + "... And Smile...", end=' ')
             RPI_Camera.resolution = (1024, 768)
             store_at = './out_img/image' + str(tm) + str(taken) + '.jpg'
             images.append(store_at)
             RPI_Camera.capture(store_at)
-            if (taken != No_of_Pictures ):
+            if (taken != No_of_Pictures):
                 print("nice one ! One More...Stay Still")
                 sleep(No_of_Pictures/2)
             else:
                 print("Great Pictures...!")
-        print("Sending Mail...",end=' ')
+        print("Sending Mail...", end=' ')
         SendMail(images)
         print("Affrimative !")
-        
+
         # Not to capture immediately
         sleep(10)
-    
-
-
-
-        
