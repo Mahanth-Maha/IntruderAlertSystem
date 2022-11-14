cdfea0704d3dad960da0172bdf7e8342af43a6d2
maha <yallamahanth2002@gmail.com>
2022-11-14 22:58:52 +0530

    sending test mail

--- ID_V2_pi/send_email_api.py
+++ ID_V2_pi/send_email_api.py
@@ -7,15 +7,15 @@ API_NAME = 'gmail'
 API_VERSION = 'v1'
 SCOPES = ['https://mail.google.com/']
 service = Create_Service(CLIENT_SECRET_FILE,API_NAME,API_VERSION,SCOPES)
-# emailMsg = 'You won $100, 000'
-# mimeMessage = MIMEMultipart()
-# mimeMessage[ 'to'] ='pics.general.backup.maha@gmail.com '
-# mimeMessage[ 'subject'] = 'You won'
+emailMsg = 'Hello\nTest 101 - Sending Email\n\nThank you\n'
+mimeMessage = MIMEMultipart()
+mimeMessage['to'] ='pics.general.backup.maha@gmail.com '
+mimeMessage['subject'] = "Hi Maha's RasberryPi Here"
 
-# mimeMessage. attach (MIMEText(emailMsg, 'plain' ) )
-# raw_string = base64.urlsafe_b64decode(mimeMessage.as_bytes().decode())
-# message = service.users().messages().send(userId='me',body={'raw':raw_string}).execute()
-# print(message)
+mimeMessage. attach (MIMEText(emailMsg, 'plain' ) )
+raw_string = base64.urlsafe_b64decode(mimeMessage.as_bytes().decode())
+message = service.users().messages().send(userId='me',body={'raw':raw_string}).execute()
+print(message)
 
 
 # pip install -upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
