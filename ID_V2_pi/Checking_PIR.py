ca5503405506e7f46c379ee60f769c63dc4bc96f
maha <yallamahanth2002@gmail.com>
2022-12-23 09:16:14 +0530

    check pir code

---
+++ ID_V2_pi/Checking_PIR.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+
+# Set up the GPIO pins for the PIR sensor
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.IN)
+
+# Set up the LED
+GPIO.setup(24, GPIO.OUT)
+
+try:
+    while True:
+        # Check the PIR sensor
+        if GPIO.input(18):
+            # Turn on the LED
+            GPIO.output(24, GPIO.HIGH)
+            print("Motion detected!")
+            # time.sleep(1)
+        else:
+            # Turn off the LED
+            GPIO.output(24, GPIO.LOW)
+            # time.sleep(0.1)
+
+except KeyboardInterrupt:
+    # Clean up the GPIO pins before exiting
+    GPIO.cleanup()
